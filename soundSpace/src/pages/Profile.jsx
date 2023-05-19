a1f350d6928e4dddfd1d8414c88ec55a75b89f9b
RuiMSMelo <ruimsmelo@gmail.com>
2023-05-19 15:38:52 +0100

    initial commit

--- soundSpace/src/pages/Profile.jsx
+++ soundSpace/src/pages/Profile.jsx
@@ -1,13 +1,18 @@
 /* import React from 'react' */
+import { useParams } from "react-router-dom"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 
 function Profile() {
+
+  { id } = useParams()
+
   return (
     <div>
-    <Navbar />
-    Profile
-    <Footer />
+      <Navbar />
+        Profile this can be deleted later
+        
+      <Footer />
     </div>
   )
 }
