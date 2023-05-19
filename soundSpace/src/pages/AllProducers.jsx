65fae9fc02adb92a176137fde43dcd5eb01bdbd7
RuiMSMelo <ruimsmelo@gmail.com>
2023-05-19 15:21:01 +0100

    basic all prod and all RL done

--- soundSpace/src/pages/AllProducers.jsx
+++ soundSpace/src/pages/AllProducers.jsx
@@ -1,13 +1,36 @@
 /* import React from 'react' */
+import axios from "axios"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 function AllProducers() {
+
+  const [producers, setProducers] = useState([])
+
+  const getAllProducers = async () => {
+    try {
+      const response = await axios.get('EXAMPLE')
+      setProducers(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect (() => {
+    getAllProducers()
+  }, [])
+
   return (
     <div>
-    <Navbar />
-    AllProducers
-    <Footer />
+      <Navbar />
+      AllProducers
+        {producers.map(oneProducer => (
+          <h3><Link to={`EXAMPLE2`}>{oneProducer.name}</Link></h3>
+        ))}
+
+      <Footer />
     </div>
   )
 }
--- soundSpace/src/pages/AllRecordLabels.jsx
+++ soundSpace/src/pages/AllRecordLabels.jsx
@@ -1,13 +1,33 @@
 /* import React from 'react' */
+import { useEffect, useState } from "react"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 
 function AllRecordLabels() {
+
+  const [recordLabels, setRecordLabels] = useState([])
+
+  const getAllRecordLabels = async () => {
+    try {
+      const response = await axios.get('EXAMPLE')
+      setRecordLabels(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+  } 
+
+  useEffect (() => {
+    getAllRecordLabels()
+  }, [])
+
   return (
     <div>
-    <Navbar />
-    AllRecordLabels
-    <Footer />
+      <Navbar />
+        AllRecordLabels
+        {recordLabels.map(oneRecordLabel => (
+          <h3><Link to={'EXAMPLE'}>{oneRecordLabel.companyName}</Link></h3>
+        ))}
+      <Footer />
     </div>
   )
 }
