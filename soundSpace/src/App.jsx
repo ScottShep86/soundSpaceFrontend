86bcb11d1b7ea5da20ad191509657eb21c657aa1
RuiMSMelo <ruimsmelo@gmail.com>
2023-05-22 18:27:09 +0100

    2nd commit after overwrite

--- soundSpace/src/App.jsx
+++ soundSpace/src/App.jsx
@@ -2,8 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LogIn from './pages/LogIn';
 import SignUpP from './pages/SignUpP';
+import SignUpRL from './pages/SignUpRL';
 import Profile from './pages/Profile';
 import EditProfile from './pages/EditProfile';
+import Review from './pages/Review';
 import AllProducers from './pages/AllProducers';
 import AllRecordLabels from './pages/AllRecordLabels';
 
@@ -12,9 +14,11 @@ function App() {
     <Routes>
       <Route path='/' element={<HomePage />}/>
       <Route path='/login' element={<LogIn />}/>
-      <Route path='/signup' element={<SignUpP />}/>
-      <Route path='/profile' element={<Profile />}/>
+      <Route path='/signup/producer' element={<SignUpP />}/>
+      <Route path='/signup/record-label' element={<SignUpRL />}/>
+      <Route path='/profile/:id' element={<Profile />}/>
       <Route path='/profile/:id/edit' element={<EditProfile />}/>
+      <Route path='/profile/review' element={<Review />}/>
       <Route path='/producers' element={<AllProducers />}/>
       <Route path='/record-labels' element={<AllRecordLabels />}/>
     </Routes>
